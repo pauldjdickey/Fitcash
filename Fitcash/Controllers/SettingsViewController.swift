23195515286764242706c0538ea913182d2d0203
Paul Dickey <pauldjdickey@gmail.com>
2019-01-07 16:02:42 -0800

    Need to add ability to choose location

--- Fitcash/Controllers/SettingsViewController.swift
+++ Fitcash/Controllers/SettingsViewController.swift
@@ -8,22 +8,49 @@
 
 import UIKit
 import Firebase
+import CoreLocation
 
-class SettingsViewController: UIViewController {
+
+class SettingsViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+   
+    
     
     let defaults = UserDefaults.standard
+    @IBOutlet weak var favoriteGymPickerView: UIPickerView!
+    
+    var pickerData: [String] = [String]()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.tabBarController?.navigationItem.leftBarButtonItem = nil
         self.tabBarController?.navigationItem.hidesBackButton = true
         self.tabBarController?.navigationItem.setHidesBackButton(true, animated: false)
+        
+        self.favoriteGymPickerView.delegate = self
+        self.favoriteGymPickerView.dataSource = self
+        
+        pickerData = ["Item 1", "Item 2", "Item 3", "Item 4", "Item 5", "Item 6"]
     }
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(true)
         tabBarController?.navigationItem.title = "Settings"
 
     }
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pickerData.count
+    }
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return pickerData[row]
+    }
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        print("\(pickerData[row])")
+    }
+
     
     @IBAction func logOutPressed(_ sender: Any) {
         do {
